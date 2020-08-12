c80f03fab7e9c2a298410b79f6c6b99eed704e66
muhammad irshad <syyed.asshi89@gmail.com>
2020-08-12 12:56:13 +0500

    firebase database

---
+++ app.js
@@ -0,0 +1,11 @@
+function saveData(){
+    var name = document.getElementById("name");
+    var roll = document.getElementById("roll");
+
+    var student = {
+        name: name.value,
+        roll: roll.value
+    }
+    firebase.database().ref('student').set(student)
+}
+
